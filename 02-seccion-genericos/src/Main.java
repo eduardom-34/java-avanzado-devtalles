2f101cd58d57a18c681a51d3fd667824eba0c7db
CesarLinux <eduardomelchor34@gmail.com>
2026-06-03 20:37:55 -0600

    medio de clase de wildcars

--- 02-seccion-genericos/src/Main.java
+++ 02-seccion-genericos/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -13,7 +14,24 @@ public class Main {
         Utility.printItem(10);
 
         MathUtils.saum(2, 2);
+        MathUtils.saum(2, 4);
 
+        List<String> names = new ArrayList<>();
+        names.add("Cesar");
+        names.add("Maria");
 
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+
+        printList(numbers);
+        printList(names);
+
+    }
+
+    public static void printList(List<?> list){
+        for (Object o: list){
+            System.out.println(o);
+        }
     }
 }
